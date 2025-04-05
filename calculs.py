f75073b1de558f09c3396fc059ac941e01897148
Steven Li <stevenliyul@gmail.com>
2025-04-05 14:28:05 -0400

    finished

--- calculs.py
+++ calculs.py
@@ -1,37 +1,105 @@
 from math import tan, pi
+from matplotlib.animation import FuncAnimation
+import matplotlib.pyplot as plt
+import numpy as np
 
-def get_series(b: float, theta: float):
-    # get exact series (we calculated that it ends up being b/(1+tan(theta))
+def graph_series(b, theta):
+    """
+    Graphical representation of the function calculated in question 1
+    """
+    x = np.arange(0, 50, 1)
+    plt.plot(
+        x, b*(1-(-tan(theta)) ** (x+1)) / (1+tan(theta))
+    )
+    plt.show()
+
+def get_convergence_point_1(b: float, theta: float):
+    """
+    Get the exact answer (we calculated that it ends up being b/(1+tan(theta))
+    """
     return b/(1+tan(theta))
 
-def tan_limit(n: int, theta: float):
-    # limt[n->inf] (tan(theta))^n
-    t = tan(theta) ** n
+def tan_limit(a:int, j: int, theta: float):
+    """
+    Calculate the limit[n->j] (tan(theta))^n
+    """
+    t = a*(-tan(theta)) ** j
 
     return t
 
-def approximate_limit_series(b: float, n: int, theta: float):
+def approximate_limit_series(a:int, b: float, j: int, theta: float):
     """
+    Approximates the point for lim approaches j for a given angle theta < pi/4
+    `a`: starting point \\
     `b`: side of the square \\
-    `n`: approximate limit -> n \\
+    `j`: approximate limit n -> j \\
     `theta`: angle
     """
-    c = 0
+    series_sum = 0
 
-    # limit[n->inf] of b(-tan)^k 
-    for n in range(0,n+1): #n+1 cuz programming >:(
-        c += b*(-tan(theta)) ** n 
+    series_sum = b*(1-(-tan(theta)) ** (j+1)) / (1+tan(theta))
 
-    t = tan_limit(n=n,theta=theta)
+    t = tan_limit(a=a, j=j,theta=theta)
 
-    return(c + t)
+    return(series_sum + t)
+
+def reflect_vectors(a: np.ndarray,b: np.ndarray):
+    """
+    Function to reflect vectors
+    """
+    r = 2 * (np.dot(a, b)/np.linalg.norm(b)**2) * b - a
+
+    return r
+
+def problem2(n, l):
+    k = 10
+    # n = number of sides
+    # l = length of one side
+    fig, ax = plt.subplots()
+    square = [
+        [
+            [0.0,float(n)],
+            [0.0,0.0],
+        ],
+        [
+            [float(n),float(n)],
+            [float(n),0.0],
+        ],
+        [
+            [0.0,float(n)],
+            [float(n),float(n)],
+        ],
+        [
+            [0.0,0.0],
+            [float(n),0.0],
+        ],
+    ]
+    for side in square:
+        ax.plot(
+            side[0], side[1], 
+        )
+
+    plt.show()
 
 if __name__ == '__main__':
-    n = 1000000
+    a = 2
+    j = 1000000
     theta = pi/6
     b = 6
+    
+    graph_series(b=b, theta=theta)
+    problem2(4, 2)
 
-    print(f"tan lim: {tan_limit(n, theta)}")
-    print(f"Limit: {approximate_limit_series(b=b, n=n, theta=theta)}")
-    print(f"b/(1 + tan): {get_series(b=b, theta=theta)}")
+    # fig, ax = plt.subplots()
+    # v1 = np.array([[0,1],[0,1]]).tolist()
+    # ax.plot(v1[0], v1[1])
+    # v2 = np.array([[0,2],[2,0]]).tolist()
+    # ax.plot(v2[0], v2[1])
+    # v3 = reflect_vectors(v1, v2).tolist()
+    # ax.plot(v3[0], v3[1])
+    # plt.show()
 
+    print(f"Tangent limit: {tan_limit(a=a, j=j, theta=theta)}")
+    for i in range(1,20):
+        print(f"Approximation: {approximate_limit_series(a=a, b=b, j=i, theta=theta)}")
+    print(f"Exact number: {get_convergence_point_1(b=b, theta=theta)}")
--- video.py
+++ video.py
@@ -1,16 +1,15 @@
 from manim import *
 from math import pi
-from calculs import approximate_limit_series, get_series
+from calculs import approximate_limit_series, get_convergence_point_1
 import numpy as np
 
 b = 6
-a = 3
+a = 2
 theta = pi/6
 
 class Scene1(Scene):
     def construct(self):
-        convergence_point = get_series(b=b, theta=theta)
-        t = ValueTracker(3)
+        convergence_point = get_convergence_point_1(b=b, theta=theta)
 
         grid = Axes(
             x_range=(0,9),
@@ -20,14 +19,13 @@ class Scene1(Scene):
         )
 
         axis_labels = grid.get_axis_labels(
-            Tex("x").scale(0.7), Text("y").scale(0.45)
+            MathTex("x").scale(0.7), MathTex("y").scale(0.7)
         )
-
 
         table = Square(side_length=6, stroke_width=4, color=GREEN, fill_opacity=0.5).shift(LEFT*1.5 + DOWN*0.5)
         ball = Dot(color=WHITE, fill_opacity=1).move_to(grid @ (a,0,0))
-        ball_transparent_initial = Dot(color=RED, fill_opacity=1).move_to(grid @ (a,0,0))
-        a_text_initial = Tex(f"a={a}").scale(0.55).next_to(grid @ (a,0,0), direction=UP)
+        ball_transparent_initial = Dot(color=YELLOW, fill_opacity=1).move_to(grid @ (a,0,0))
+        a_text_initial = MathTex(f"a={a}").scale(0.55).next_to(grid @ (a,0,0), direction=UP)
 
         self.play(Create(grid, run_time=1))
         self.add(axis_labels)
@@ -37,31 +35,58 @@ class Scene1(Scene):
         self.play(FadeIn(a_text_initial))
         run_time = 2
 
+        LINE_WIDTH = 4
+
+        # FIRST PART OF THE ANIMATION
         side = 1
         previous_x = a
         previous_y = 0
         for i in range(1,5):
+
+            approximated_value = approximate_limit_series(a=a, b=b, j=i, theta=theta)
+
             run_time -= 0.2
             if run_time <= 0:
                 run_time = 0.1
 
-            if side == 1: # Bottom side
+            if side == 1: # Right side
                 x = b
-                y = approximate_limit_series(b, i, theta)
-            if side == 2:
-                x = b-approximate_limit_series(b, i, theta)
+                y = approximated_value
+                line1 = Line(grid @ (x,0), grid @ (x,y), stroke_width=LINE_WIDTH, stroke_color=RED)
+                line2 = Line(grid @ (a,0), grid @ (b,0), stroke_width=LINE_WIDTH, stroke_color=BLUE)
+                line1_text = MathTex(f"r_{side}").next_to(line1, RIGHT).scale(0.5).set_color(RED)
+                line2_text = MathTex("(b-a)").next_to(line2, DOWN, buff=0.1).scale(0.5).set_color(BLUE)
+            if side == 2: # Top side 
+                x = b-approximated_value
                 y = b
-            if side == 3:
+                line1 = Line(grid @ (x,b), grid @ (b,b), stroke_width=LINE_WIDTH, stroke_color=RED)
+                line2 = Line(grid @ (b,previous_y), grid @ (b,y), stroke_width=LINE_WIDTH, stroke_color=BLUE)
+                line1_text = MathTex(f"r_{side}").next_to(line1, UP).scale(0.5).set_color(RED)
+                line2_text = MathTex("b-r_1").next_to(line2, RIGHT, buff=0.1).scale(0.5).set_color(BLUE)
+            if side == 3: #Left Side
                 x = 0
-                y = b-approximate_limit_series(b, i, theta)
-            if side == 4:
-                x = approximate_limit_series(b, i, theta)
+                y = approximated_value
+                line1 = Line(grid @ (x,b), grid @ (previous_x,b), stroke_width=LINE_WIDTH, stroke_color=BLUE)
+                line2 = Line(grid @ (0,b), grid @ (0,y), stroke_width=LINE_WIDTH, stroke_color=RED)
+                line1_text = MathTex(f"r_{side}").next_to(line1, LEFT).scale(0.5).set_color(RED)
+                line2_text = MathTex("b-r_2").next_to(line2, UP, buff=0.1).scale(0.5).set_color(BLUE)
+            if side == 4: # Bottom side
+                x = approximated_value
                 y = 0
+                line1 = Line(grid @ (previous_x,0), grid @ (x,0), stroke_width=LINE_WIDTH, stroke_color=RED)
+                line2 = Line(grid @ (0,previous_y), grid @ (0,0), stroke_width=LINE_WIDTH, stroke_color=BLUE)
+                line1_text = MathTex(f"r_{side}").next_to(line1, DOWN).scale(0.5).set_color(BLUE)
+                line2_text = MathTex("b-r_3").next_to(line2, LEFT, buff=0.1).scale(0.5).set_color(RED)
                 side = 0
             
             new_pos = grid.coords_to_point(x, y)
             self.play(ball.animate.move_to(new_pos), run_time=run_time)
             grid += Dot(color=WHITE, fill_opacity=0.5).move_to(grid @ (x,y,0))
+            self.add(line1)
+            self.add(line2)
+            self.add(line1_text)
+            self.add(line2_text)
+
             trail = Line(grid @ (previous_x,previous_y), grid @ (x,y), stroke_opacity=0.5, stroke_width=1)
             grid += trail
             side += 1
@@ -69,27 +94,28 @@ class Scene1(Scene):
             previous_y = y
 
         self.wait(3)
-
+        
+        # SECOND PART OF THE ANIMATION
         side = 1
         run_time = 1.5
-        previous_x = a
-        previous_y = 0
-        for i in range(4,52):
+        for i in range(5,52):
             run_time -= 0.2
             if run_time <= 0:
                 run_time = 0.1
-
+            
+            approximated_value = approximate_limit_series(a=a, b=b, j=i, theta=theta)
+            
             if side == 1: # Bottom side
                 x = b
-                y = approximate_limit_series(b, i, theta)
+                y = approximated_value
             if side == 2:
-                x = b-approximate_limit_series(b, i, theta)
+                x = b-approximated_value
                 y = b
             if side == 3:
                 x = 0
-                y = b-approximate_limit_series(b, i, theta)
+                y = b-approximated_value
             if side == 4:
-                x = approximate_limit_series(b, i, theta)
+                x = approximated_value
                 y = 0
                 side = 0
 
@@ -101,8 +127,7 @@ class Scene1(Scene):
             previous_x = x
             previous_y = y
         
-        a_text_final = Tex(f"a={round(convergence_point, 3)}...").scale(0.55).next_to(grid @ (convergence_point,0,0), direction=DOWN)
-        self.play(FadeIn(a_text_final))
-        self.wait(3)
-
-            
+        final_point = Dot().move_to(grid @ (convergence_point,0,0))
+        a_text_final = MathTex(f"{round(convergence_point, 4)}...").scale(0.55).next_to(final_point, direction=UP)
+        self.play(Write(a_text_final), run_time=1)
+        self.wait(4)
